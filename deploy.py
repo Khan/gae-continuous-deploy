0af47846922f852f61de94bcbea5d98bf6e996bc
David Hu <davidhu91@gmail.com>
2012-07-29 21:56:18 -0700

    make install_deps instead of requirements.txt

--- deploy.py
+++ deploy.py
@@ -83,8 +83,7 @@ def deploy_to_staging():
     update_repo()
     decrypt_secrets()
     shutil.copy2("secrets_dev.py", REPO_DIR)
-    subprocess.check_call(["pip", "install", "-r", "requirements.txt"],
-            cwd=REPO_DIR)
+    subprocess.check_call(["make", "install_deps"], cwd=REPO_DIR)
 
     print "Deploying!"
 
