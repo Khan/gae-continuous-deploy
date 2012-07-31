0e8a977a11a83e7d997ee9f048eec0cea9a8fea7
David Hu <davidhu91@gmail.com>
2012-07-31 13:28:35 -0700

    Add a quick and dirty start script just using nohup for Mr Deploy
    Test Plan: ran it
    
    Reviewers: benkomalo
    
    Reviewed By: benkomalo
    
    Differential Revision: http://phabricator.khanacademy.org/D490

---
+++ start_daemon.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Runs deploy.py as a daemon.
+
+# TODO(david): Actually run as a daemon, this is quick'n'dirty nohup right now.
+
+ln -sfnv "$(pwd)/deploy.log" $HOME
+nohup python deploy.py > deploy.log &
